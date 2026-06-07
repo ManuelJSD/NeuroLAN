7505a72f62a770af6e58e7093445e1a74a365667
ManuelJSD <manueljsandalio@gmail.com>
2026-06-07 18:57:23 +0200

    feat: implement chat interface with model selection and message interaction logic

--- src/app/pages/chat/chat.page.ts
+++ src/app/pages/chat/chat.page.ts
@@ -1,28 +1,34 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonSelect, IonList, IonItem, IonSelectOption } from "@ionic/angular/standalone";
+import { IonSelect, IonList, IonItem, IonSelectOption, IonContent, IonLabel, IonButton, IonTextarea } from "@ionic/angular/standalone";
 import { LmStudioService } from 'src/app/core/services/lm-studio';
-import { LmStudioModel } from 'src/app/core/models/lmstudio.model';
+import { ChatMessage, LmStudioModel } from 'src/app/core/models/lmstudio.model';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.page.html',
   styleUrls: ['./chat.page.scss'],
   standalone: true,
-  imports: [
+  imports: [IonButton, IonLabel, IonContent,
     CommonModule,
     FormsModule,
     IonSelect,
     IonList,
     IonItem,
-    IonSelectOption
+    IonSelectOption,
+    IonTextarea
   ],
 })
 export class ChatPage implements OnInit {
   private lmStudioService = inject(LmStudioService);
   models: LmStudioModel[] = [];
   selectedModelKey: string | undefined;
+
+  messages: ChatMessage[] = [];
+  userInput: string = '';
+  isSending = false;
+  errorMessage: String | null = null;
 
   ngOnInit(): void {
     this.loadModels();
@@ -37,6 +43,27 @@ export class ChatPage implements OnInit {
       },
       error: (err) => console.error(err),
     })
+  }
+
+  sendMessage() {
+
+    const userText = this.userInput.trim();
+
+    // ¿Texto vacio? Salir
+    if (userText === '') return;
+
+    //Insermos el mensaje del usuario
+    this.messages.push({
+      role: 'user',
+      content: userText
+    });
+
+    //Respuesta dummy
+    this.messages.push({
+      role: 'assistant',
+      content: 'Hola'
+    })
+
   }
 
 }
